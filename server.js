38f62409f00a745f83c394c4e0718ffc761cb7c6
aradhya <singh.aradhya2019@gmail.com>
2022-06-05 14:53:56 +0530

    heroku

---
+++ server.js
@@ -0,0 +1,50 @@
+//jshint esversion:6
+
+const exp = require("express");
+const bp = require("body-parser");
+const app = exp();
+
+app.use(bp.urlencoded({ extended: true }));
+app.use(exp.static("public"));
+app.set("view engine", "ejs");
+
+var items = ["eat", "netflix", "sleep"];
+var workItems = [];
+
+app.get("/", (req, res) => {
+  var options = {
+    day: "numeric",
+    weekday: "long",
+    month: "short",
+  };
+  var today = new Date();
+  const day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { title: day, newI: items });
+  //coz of this newI is also array and newI = items i.e contents of newI same of items
+});
+
+app.post("/", (req, res) => {
+  /*after pressing + , console.log(req.body) will give {newI:'text entered' ,btnName:'value of btn'} 
+     therefore req.body.btnName will give value stored in button*/
+  let ni = req.body.newItem;
+  if (req.body.btnName === "Work") {   /* if we write ==="Work List", then why if is not working and all work items are posted in home pg */
+    workItems.push(ni);
+    res.redirect("/2");
+  } else {
+    items.push(ni);
+    res.redirect("/");
+  }
+});
+
+app.get("/2", (req, res) => {
+  res.render("list", { title: "Work List", newI: workItems });
+});
+
+app.get("/about",(req,res)=>{
+  res.render("about")
+})
+
+app.listen(process.env.PORT||3000, () => {
+  console.log("started");
+});
